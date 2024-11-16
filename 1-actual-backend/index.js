29a1fd24103adf23384050d21a37b192dec657ab
Krithik983 <rajkrithik4@gmail.com>
2024-11-16 17:16:08 +0530

    Updated the code

--- 1-actual-backend/index.js
+++ 1-actual-backend/index.js
@@ -1,9 +1,7 @@
-//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
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey:
-    "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", // Replace with your OpenAI API key
+  apiKey: "", // Replace with your OpenAI API key
 });
 
 async function main() {
